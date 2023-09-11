ef0e86981302f81b87cab77b184d1a458bfb361a
Daniel Zhang <daniel.zhang9876@gmail.com>
2023-09-11 16:32:26 -0400

    finished lab 1

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int num = 100;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+                steps += 1;
+            }
+            else {
+                num -= 1;
+                steps += 1;
+            }
+        }
+        System.out.println(steps);
+    }
+}
